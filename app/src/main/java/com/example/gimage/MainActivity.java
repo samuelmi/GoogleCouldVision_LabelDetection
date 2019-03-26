7403bab8b93db09a6b5b94e7653da935e50fcd06
Nirjon <nirjon@gmail.com>
2019-03-25 20:02:59 -0400

    Initial commit

---
+++ app/src/main/java/com/example/gimage/MainActivity.java
@@ -0,0 +1,86 @@
+package com.example.gimage;
+
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.fasterxml.jackson.core.JsonFactory;
+import com.fasterxml.jackson.core.JsonParser;
+import com.google.api.client.extensions.android.http.AndroidHttp;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.json.gson.GsonFactory;
+import com.google.api.services.vision.v1.Vision;
+import com.google.api.services.vision.v1.VisionRequestInitializer;
+import com.google.api.services.vision.v1.model.AnnotateImageRequest;
+import com.google.api.services.vision.v1.model.AnnotateImageResponse;
+import com.google.api.services.vision.v1.model.BatchAnnotateImagesRequest;
+import com.google.api.services.vision.v1.model.BatchAnnotateImagesResponse;
+import com.google.api.services.vision.v1.model.Feature;
+import com.google.api.services.vision.v1.model.Image;
+import com.google.gson.Gson;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    myVisionTester();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+
+    }
+
+    void myVisionTester() throws IOException
+    {
+        //1. ENCODE image.
+        Bitmap bitmap = ((BitmapDrawable)getResources().getDrawable(R.drawable.b1)).getBitmap();
+        ByteArrayOutputStream bout = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 90, bout);
+        Image myimage = new Image();
+        myimage.encodeContent(bout.toByteArray());
+
+        //2. PREPARE AnnotateImageRequest
+        AnnotateImageRequest annotateImageRequest = new AnnotateImageRequest();
+        annotateImageRequest.setImage(myimage);
+        Feature f = new Feature();
+        f.setType("LABEL_DETECTION");
+        f.setMaxResults(5);
+        List<Feature> lf = new ArrayList<Feature>();
+        lf.add(f);
+        annotateImageRequest.setFeatures(lf);
+
+        //3.BUILD the Vision
+        HttpTransport httpTransport = AndroidHttp.newCompatibleTransport();
+        GsonFactory jsonFactory = GsonFactory.getDefaultInstance();
+        Vision.Builder builder = new Vision.Builder(httpTransport, jsonFactory, null);
+        builder.setVisionRequestInitializer(new VisionRequestInitializer("AIzaSyCuiWsRujAdUWwG0sb4Y13ul466IJMHoFM"));
+        Vision vision = builder.build();
+
+        //4. CALL Vision.Images.Annotate
+        BatchAnnotateImagesRequest batchAnnotateImagesRequest = new BatchAnnotateImagesRequest();
+        List<AnnotateImageRequest> list = new ArrayList<AnnotateImageRequest>();
+        list.add(annotateImageRequest);
+        batchAnnotateImagesRequest.setRequests(list);
+        Vision.Images.Annotate task = vision.images().annotate(batchAnnotateImagesRequest);
+        BatchAnnotateImagesResponse response = task.execute();
+        Log.v("MYTAG", response.toPrettyString());
+    }
+
+}
